93068174008fef6b08acabd5558c195118da8a78
Cameron Warner <warnecam@mail.gvsu.edu>
2013-02-04 14:59:25 -0500

    Initialized the java proj

---
+++ src/pkg367proj1/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg367proj1;
+
+/**
+ *
+ * @author warnecam
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
