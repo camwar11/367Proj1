19de49b23e197c12be1ca1ec152f5abc8e486714
Eric Munson <munsone@mail.gvsu.edu>
2013-02-11 13:17:28 -0500

    Cube side in 2d

---
+++ src/pkg367proj1/TestCube.java
@@ -0,0 +1,62 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg367proj1;
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLEventListener;
+import javax.media.opengl.GLProfile;
+import javax.media.opengl.GLCapabilities;
+import javax.media.opengl.awt.GLCanvas;
+
+import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+/**
+ *
+ * @author warnecam
+ */
+public class TestCube {
+        public static void main( String [] args ) {
+        GLProfile glprofile = GLProfile.getDefault();
+        GLCapabilities glcapabilities = new GLCapabilities( glprofile );
+        final GLCanvas glcanvas = new GLCanvas( glcapabilities );
+
+        glcanvas.addGLEventListener( new GLEventListener() {
+            
+            @Override
+            public void reshape( GLAutoDrawable glautodrawable, int x, int y, int width, int height ) {
+                OneTriangle.setup( glautodrawable.getGL().getGL2(), width, height );
+            }
+            
+            @Override
+            public void init( GLAutoDrawable glautodrawable ) {
+            }
+            
+            @Override
+            public void dispose( GLAutoDrawable glautodrawable ) {
+            }
+            
+            @Override
+            public void display( GLAutoDrawable glautodrawable ) {
+                System.out.println("glautodrawable.getWidth() = "+glautodrawable.getWidth());
+                                System.out.println("glautodrawable.getHeight() = "+glautodrawable.getHeight());
+
+                CubeSide.render( glautodrawable.getGL().getGL2(), 1000, 700);//glautodrawable.getWidth(), glautodrawable.getHeight() );
+            }
+        });
+
+        final Frame frame = new Frame( "One Triangle AWT" );
+        frame.add( glcanvas );
+        frame.addWindowListener( new WindowAdapter() {
+            public void windowClosing( WindowEvent windowevent ) {
+                frame.remove( glcanvas );
+                frame.dispose();
+                System.exit( 0 );
+            }
+        });
+
+        frame.setSize( 1000,700);//640, 480 );
+        frame.setVisible( true );
+    }
+}
